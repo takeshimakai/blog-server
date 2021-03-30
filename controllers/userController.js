a535db75f41a5c109a3f66ac18a1e24cdac14488
Kai Takeshima <takeshimakai@gmail.com>
2021-03-30 17:52:39 -0400

    validate sign up form

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,11 +11,61 @@ exports.getAdminProfile = (req, res, next) => {
   });
 };
 
-exports.postSignUpForm = (req, res, next) => {
-  new User({
-    email: req.body.email,
-    password: req.body.password,
-    username: req.body.username,
-    isAdmin: false
-  }).save();
-};
+exports.postSignUpForm = [
+  body('email')
+  .trim()
+  .notEmpty()
+  .withMessage('Email must be specified')
+  .toLowerCase()
+  .isEmail()
+  .withMessage('Please enter a valid email')
+  .custom(value => {
+    return User.findOne({ email: value }).then(user => {
+      if (user) {
+        return Promise.reject('E-mail already in use');
+      };
+    });
+  }),
+
+  body('password')
+  .trim()
+  .notEmpty()
+  .withMessage('Password must be specified'),
+
+  body('username')
+  .trim()
+  .notEmpty()
+  .withMessage('Username must be specified')
+  .isAlphanumeric()
+  .withMessage('Username must consist of letters and numbers')
+  .custom(value => {
+    return User.findOne({ username: value }).then(user => {
+      if (user) {
+        return Promise.reject('Username already taken');
+      };
+    });
+  }),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.send(errors);
+    } else {
+      bcrypt.hash(req.body.password, 10, (err, hashedPw) => {
+        if (err) return next(err);
+  
+        new User({
+          email: req.body.email,
+          password: hashedPw,
+          username: req.body.username,
+          isAdmin: false
+        }).save(err => {
+          if (err) {
+            res.send('Something went wrong. Unable to create new user.');
+          }
+        });
+      });
+    };
+  }
+];
